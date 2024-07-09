6b49052323423408e3d88a845ac6c858a1357185
quantenschaum <woxpox@posteo.de>
2024-07-09 11:10:54 +0200

    improved wind history plot widget
    - added color code for TWA
    - do not smooth AW
    - cleanup

--- Sail_Instrument/plugin.py
+++ Sail_Instrument/plugin.py
@@ -480,8 +480,8 @@ class Plugin(object):
 
                 data = d = CourseData(**data)  # compute missing values
 
-                # self.smooth(data, "AWD", "AWS")
-                # data["AWAF"] = to360(data["AWDF"] - data["HDT"]) if d.has("AWDF", "HDT") else None
+                self.smooth(data, "AWD", "AWS")
+                data["AWAF"] = to360(data["AWDF"] - data["HDT"]) if d.has("AWDF", "HDT") else None
                 self.smooth(data, "TWD", "TWS")
                 data["TWAF"] = to180(data["TWDF"] - data["HDT"]) if d.has("TWDF", "HDT") else None
                 self.smooth(data, "SET", "DFT")
